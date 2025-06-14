3b1a371c66616089ef65870f27f82e226f4bac52
Pukkok <krystalgggg@gmail.com>
2025-06-15 00:44:43 +0900

    [Fix] 포트 실행 추가

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
 const app = require('../src/app') // ⬅ alias 대신 상대경로
-module.exports = app
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+    console.log(`${PORT}번 연결`)
+})
+
+// module.exports = app
