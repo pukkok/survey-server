1552de1117c11f384e3227ae083894bc52e5db5e
Pukkok <krystalgggg@gmail.com>
2025-06-15 00:16:51 +0900

    [Fix] 개발, 배포환경 분리

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 require('module-alias/register')  // ✅ 반드시 맨 위에 있어야 함
 
 const app = require('@/app')
+const PORT = 8080
+
+app.listen(PORT, () => {
+    console.log(`${PORT}번 연결`)
+})
 
 module.exports = app
