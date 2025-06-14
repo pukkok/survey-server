4ccc4b849df437e0cf45dd47ee1bd6b1ecd9f8d8
Pukkok <krystalgggg@gmail.com>
2025-06-14 23:47:49 +0900

    [Fix] 포트 번호 변경

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 require('module-alias/register')  // ✅ 반드시 맨 위에 있어야 함
 
 const app = require('@/app')
-const PORT = 5454
+const PORT = 8080
 
 app.listen(PORT, () => {
     console.log(`${PORT}번 연결`)
